8fa5a46eaf1aeedbebbd1474997f8d13027da9a9
Kimi <kay351601@aliyun.com>
2018-10-13 00:32:27 +0800

    just commit

--- chal.py
+++ chal.py
@@ -9,14 +9,19 @@ for n in es_dict:
     es = es_dict[n]
     if es:
         e = es[0]
-        #if e.end_at is None and e.etype == 'chal':
-        if e.end_at is None:
+        if e.end_at is None and e.etype == 'chal':
              host_id, node_id = e.location
              home = e.home
-             if host_id in [17, 18]:
+             if host_id in [17, 18] or host_id > 40:
                  continue
+                 
              if n in ns_dict:
-                 print(ns_dict[n], e)
+                 if 30 * 60 * 1000 < e.duration < 110 * 60 * 1000:
+                     print(ns_dict[n], e)
+                     #do_challenge(host_id, node_id)
+               
+
+
              #  # snset(host_id, node_id, 'home', 'ts2.eu')
              #  snset(host_id, node_id, 'rpchost', 'bibenwei.com')
              #  if host_id == 28:
