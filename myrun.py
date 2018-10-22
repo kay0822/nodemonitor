120537e40c0a60d8e988bad732c869b8204b5036
kimi <kay351601@aliyun.com>
2018-10-22 13:20:46 +0800

    just commit

--- myrun.py
+++ myrun.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 from monitor import Monitor
+import personal
 
 if __name__ == '__main__':
     m = Monitor(
+        apikeys=personal.MY,
+        enable_super=True,
         only=[1] + list(range(31, 34 + 1)) + list(range(36, 55 + 1)),
         exclude=[17, 18]
     )
